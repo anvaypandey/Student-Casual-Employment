6aae1a2b9e7fc134175aa5d59092abfd7104d92f
Bianca Negoita <BIANCA@192-168-1-110.tpgi.com.au>
2020-05-23 15:08:53 +1000

    resolved conflicts

--- src/TestEmployerConsole.java
+++ src/TestEmployerConsole.java
@@ -1,89 +1,89 @@
-//import static org.junit.Assert.*;
-//
-//import java.util.ArrayList;
-//
-//import org.junit.After;
-//import org.junit.AfterClass;
-//import org.junit.Before;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//
-//public class TestEmployerConsole extends EmployerConsole {
-//
-//	EmployerConsole emp;
-//
-//	@BeforeClass
-//	public static void setUpBeforeClass() throws Exception {
-//
-//	}
-//
-//	@AfterClass
-//	public static void tearDownAfterClass() throws Exception {
-//	}
-//
-//	@Before
-//	public void setUp() throws Exception {
-//
-//
-//
-//		MainConsole.userList.put("student1", new Student("student1","123","student1@email.com",Availability.FullTime));
-//		MainConsole.userList.put("student2", new Student("student2","123","student2@email.com",Availability.PartTime));
-//		//MainConsole.userList.put("student3", new Student("student3","123","student3",Availability.PartTime));
-//		//MainConsole.userList.put("student4", new Student("student4","123","student4",Availability.FullTime));
-//		//MainConsole.userList.put("student5", new Student("student5","123","student5",Availability.Internship));
-//		//MainConsole.userList.put("student6", new Student("student6","123","student6",null));
-//
-//		MainConsole.jobCategories.add("Engineering");
-//		MainConsole.jobCategories.add("Accounting");
-//
-//		((Student)MainConsole.userList.get("student1")).addJobCategory("Engineering");
-//		((Student)MainConsole.userList.get("student2")).addJobCategory("Accounting");
-//
-//		MainConsole.userList.put("emp123", new Employer("emp123","123","student1@email.com"));
-//
-//
-//	}
-//
-//	@After
-//	public void tearDown() throws Exception {
-//	}
-//
-//	@Test (expected = Exception.class)
-//	public void testSearchApplicantsbyAvailability1() throws Exception {
-//		searchApplicantsbyAvailability(3);
-//	}
-//
-//	@Test
-//	public void testSearchApplicantsbyAvailability2() throws Exception{
-//		assertTrue(searchApplicantsbyAvailability(1));
-//	}
-//
-//	@Test (expected = Exception.class)
-//	public void testSearchApplicantsbyJobPreference() throws Exception{
-//		searchApplicantsbyJobPreference("sdfgsf");
-//	}
-//
-//	@Test
-//	public void testSearchApplicantsbyJobPreference1() throws Exception{
-//		assertTrue(searchApplicantsbyJobPreference("Engineering"));
-//	}
-//
-//	@Test
-//	public void testAddNewJob() {
-//		Job job1 = new Job("JOB001", ((Employer) MainConsole.userList.get("emp123")), "Sample Job1");
-//		Job job2 = new Job("JOB002", ((Employer) MainConsole.userList.get("emp123")), "Sample Job2");
-//		Job job3 = new Job("JOB003", ((Employer) MainConsole.userList.get("emp123")), "Sample Job3");
-//
-//		MainConsole.jobListings.add(job1);
-//		MainConsole.jobListings.add(job2);
-//		MainConsole.jobListings.add(job3);
-//
-//		Job job = new Job("JOB000", ((Employer) MainConsole.userList.get("emp123")), "Sample Job");
-//		addNewJob(job);
-//
-//		assertEquals(4, MainConsole.jobListings.size());
-//	}
-//
-//
-//
-//}
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestEmployerConsole extends EmployerConsole {
+
+	EmployerConsole emp;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+
+
+
+		MainConsole.userList.put("student1", new Student("student1","123","student1@email.com",Availability.FullTime));
+		MainConsole.userList.put("student2", new Student("student2","123","student2@email.com",Availability.PartTime));
+		//MainConsole.userList.put("student3", new Student("student3","123","student3",Availability.PartTime));
+		//MainConsole.userList.put("student4", new Student("student4","123","student4",Availability.FullTime));
+		//MainConsole.userList.put("student5", new Student("student5","123","student5",Availability.Internship));
+		//MainConsole.userList.put("student6", new Student("student6","123","student6",null));
+
+		MainConsole.jobCategories.add("Engineering");
+		MainConsole.jobCategories.add("Accounting");
+
+		((Student)MainConsole.userList.get("student1")).addJobCategory("Engineering");
+		((Student)MainConsole.userList.get("student2")).addJobCategory("Accounting");
+
+		MainConsole.userList.put("emp123", new Employer("emp123","123","student1@email.com"));
+
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test (expected = Exception.class)
+	public void testSearchApplicantsbyAvailability1() throws Exception {
+		searchApplicantsbyAvailability(3);
+	}
+
+	@Test
+	public void testSearchApplicantsbyAvailability2() throws Exception{
+		assertTrue(searchApplicantsbyAvailability(1));
+	}
+
+	@Test (expected = Exception.class)
+	public void testSearchApplicantsbyJobPreference() throws Exception{
+		searchApplicantsbyJobPreference("sdfgsf");
+	}
+
+	@Test
+	public void testSearchApplicantsbyJobPreference1() throws Exception{
+		assertTrue(searchApplicantsbyJobPreference("Engineering"));
+	}
+
+	@Test
+	public void testAddNewJob() {
+		Job job1 = new Job("JOB001", ((Employer) MainConsole.userList.get("emp123")), "Sample Job1");
+		Job job2 = new Job("JOB002", ((Employer) MainConsole.userList.get("emp123")), "Sample Job2");
+		Job job3 = new Job("JOB003", ((Employer) MainConsole.userList.get("emp123")), "Sample Job3");
+
+		MainConsole.jobListings.add(job1);
+		MainConsole.jobListings.add(job2);
+		MainConsole.jobListings.add(job3);
+
+		Job job = new Job("JOB000", ((Employer) MainConsole.userList.get("emp123")), "Sample Job");
+		addNewJob(job);
+
+		assertEquals(4, MainConsole.jobListings.size());
+	}
+
+
+
+}
--- src/TestRemoveFromBlacklist.java
+++ src/TestRemoveFromBlacklist.java
@@ -1,60 +1,60 @@
-//import static org.junit.Assert.*;
-//
-//import java.io.ByteArrayInputStream;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//public class TestRemoveFromBlacklist {
-//
-//	MaintenanceConsole main;
-//
-//	@Before
-//	public void setUp() throws Exception {
-//		MainConsole.userList.put("s1", new Student("s1","qwerty", "s1@gmail.com", Availability.PartTime));
-//		MainConsole.userList.put("s2", new Student("s2","qwert", "s2@gmail.com", Availability.PartTime));
-//		MainConsole.userList.get("s1").setBlacklistStatus(BlacklistStatus.PROVISIONAL);
-//		MainConsole.userList.get("s2").setBlacklistStatus(BlacklistStatus.FULL);
-//		String simulatedUserInput = "s1";//this is to stimulate userInput
-//		System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
-//		main = new MaintenanceConsole();
-//
-//	}
-//
-//	//test if user can be removed from provisional blacklist
-//	@Test
-//	public void removeFromProvisionalBlacklist() throws InvalidInputException, AuthorizationException {
-//
-//		main.removeFromBlacklist();
-//
-//		assertEquals(BlacklistStatus.NONE, (MainConsole.userList.get("s1").getBlacklistStatus()));
-//
-//	}
-//
-//	//test if Authorization exception would be thrown if user that needs to be removed has been on list for fewer than 90 days
-//	@Test(expected = AuthorizationException.class)
-//	public void testInvalidInputException() throws InvalidInputException, AuthorizationException {
-//
-//		String simulatedUserInput = "s2" + System.getProperty("line.separator");
-//		System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
-//
-//			main.removeFromBlacklist();
-//
-//	}
-//
-//	//test if user can be removed full blacklist if been on list for more than 90 days
-//	@Test
-//	public void removeFromFullBlacklist() throws InvalidInputException, AuthorizationException {
-//
-//		DateTime.setAdvance(90, 0, 0);
-//		String simulatedUserInput = "s2" + System.getProperty("line.separator");
-//		System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
-//		main.removeFromBlacklist();
-//
-//		assertEquals(BlacklistStatus.NONE, (MainConsole.userList.get("s2").getBlacklistStatus()));
-//
-//	}
-//
-//
-//
-//}
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestRemoveFromBlacklist {
+
+	MaintenanceConsole main;
+
+	@Before
+	public void setUp() throws Exception {
+		MainConsole.userList.put("s1", new Student("s1","qwerty", "s1@gmail.com", Availability.PartTime));
+		MainConsole.userList.put("s2", new Student("s2","qwert", "s2@gmail.com", Availability.PartTime));
+		MainConsole.userList.get("s1").setBlacklistStatus(BlacklistStatus.PROVISIONAL);
+		MainConsole.userList.get("s2").setBlacklistStatus(BlacklistStatus.FULL);
+		String simulatedUserInput = "s1";//this is to stimulate userInput
+		System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
+		main = new MaintenanceConsole();
+
+	}
+
+	//test if user can be removed from provisional blacklist
+	@Test
+	public void removeFromProvisionalBlacklist() throws InvalidInputException, AuthorizationException {
+
+		main.removeFromBlacklist();
+
+		assertEquals(BlacklistStatus.NONE, (MainConsole.userList.get("s1").getBlacklistStatus()));
+
+	}
+
+	//test if Authorization exception would be thrown if user that needs to be removed has been on list for fewer than 90 days
+	@Test(expected = AuthorizationException.class)
+	public void testInvalidInputException() throws InvalidInputException, AuthorizationException {
+
+		String simulatedUserInput = "s2" + System.getProperty("line.separator");
+		System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
+
+			main.removeFromBlacklist();
+
+	}
+
+	//test if user can be removed full blacklist if been on list for more than 90 days
+	@Test
+	public void removeFromFullBlacklist() throws InvalidInputException, AuthorizationException {
+
+		DateTime.setAdvance(90, 0, 0);
+		String simulatedUserInput = "s2" + System.getProperty("line.separator");
+		System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
+		main.removeFromBlacklist();
+
+		assertEquals(BlacklistStatus.NONE, (MainConsole.userList.get("s2").getBlacklistStatus()));
+
+	}
+
+
+
+}
