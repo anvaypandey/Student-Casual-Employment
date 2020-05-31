95146122eb9a7d720fa6ae80dce693d5490a6a7c
karen <karentkcheung@gmail.com>
2020-05-31 21:28:39 +1000

    test cases update

--- src/TestBlackListUser.java
+++ src/TestBlackListUser.java
@@ -17,34 +17,34 @@ public class TestBlackListUser {
 		MainConsole.userList.put("s1", new Student("s1","qwerty", "s1@gmail.com", Availability.PartTime));
 		MainConsole.userList.put("s2", new Student("s2","qwert", "s2@gmail.com", Availability.PartTime));
 		((Student) MainConsole.userList.get("s1")).setBlacklistStatus(BlacklistStatus.PROVISIONAL);
-		String simulatedUserInput = "s1" + System.getProperty("line.separator")+ "Y";//this is to stimulate userInput
+		
+		//this is to stimulate user inputs in the loop
+		String simulatedUserInput = "s2" + System.getProperty("line.separator") + "s1" + System.getProperty("line.separator") +"Y" + System.getProperty("line.separator");
 
 
 		System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
 		main = new MaintenanceConsole();
 
 	}
-	// positive test - test if user on provisional blacklist can be moved to full blacklist
+	
+	
 	@Test
-	public void blacklistUser() throws InvalidInputException, AuthorizationException {
+	public void testIfUserCanBeBlacklisted() throws InvalidInputException, AuthorizationException {
 
 		main.blackListUser();
-
+		
+		//Negative test - user shouldn't be blacklisted 
+		assertEquals(BlacklistStatus.NONE, (((Student) MainConsole.userList.get("s2")).getBlacklistStatus()));
+		
+		// positive test - test if user on provisional blacklist can be moved to full blacklist
 		assertEquals(BlacklistStatus.FULL, (((Student) MainConsole.userList.get("s1")).getBlacklistStatus()));
 
-	}
-
-//	Negative test - Test if exception would be thrown if user is not on provisional blacklist
-
-	@Test(expected = InvalidInputException.class)
-	public void testInvalidInputException() throws InvalidInputException, AuthorizationException {
-//		String simulatedUserInput = "s2";
-//		System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
-
-		main.blackListUser();
+		
 	}
 
 }
+	
+
 
 
 
